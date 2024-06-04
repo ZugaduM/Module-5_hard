b13bf70c62d153c675c21487cdf6c83c4f10cddd
Ilia Belyaev <144899352+ZugaduM@users.noreply.github.com>
2024-06-04 10:27:28 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import engine
+
+
+ur = engine.UrTube()
+v1 = engine.Video('Лучший язык программирования 2024 года', 200)
+v2 = engine.Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+
+ur.add(v1, v2)
+
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
+
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
+
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
+
+ur.watch_video('Лучший язык программирования 2024 года!')
